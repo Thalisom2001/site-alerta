77230888207fd59ea1598c2ddcc877ebce976f98
thalisom2001 <thalisom.almeida@aluno.uepa.br>
2025-06-24 23:19:10 -0300

    Atualiza bairros e layout

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,12 +12,12 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/5368/5368327.png' // atualizado conforme instruções
+    icon: 'https://cdn-icons-png.flaticon.com/512/5368/5368327.png'
   });
 });
